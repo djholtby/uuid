9f10a44140c689e744e1c17e56009a1b0863a35e
Dan Holtby <djholtby@gmail.com>
2019-10-25 12:32:02 -0400

    update post-install to not fail on reinstall

--- post-install.rkt
+++ post-install.rkt
@@ -4,5 +4,6 @@
 (provide post-installer)
 
 (define (post-installer collect-dir dir)
-  (with-output-to-file (build-path dir "node.rktd")
-    (lambda () (writeln (random-node)))))
+  (let ([f (open-output-file (build-path dir "node.rktd") #:exists 'replace)])
+    (writeln (random-node) f)
+    (close-output-port f)))
